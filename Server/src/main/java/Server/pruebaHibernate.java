ab0bf41627dffad1dcbdd605f7bdb7307ff6ecdf
Fiser <fiser12@icloud.com>
2016-10-09 04:36:43 +0200

    Ya funciona hibernate pero no con mi modelo, lo adaptaré

--- Server/src/main/java/Server/pruebaHibernate.java
+++ Server/src/main/java/Server/pruebaHibernate.java
@@ -1,24 +1,40 @@
 package Server;
 
-import Server.Model.Habitacion;
+import java.util.Date;
 import org.hibernate.Session;
+import Server.Model.Category;
+import Server.Model.Stock;
+import Server.Model.StockCategory;
 
-import java.sql.Date;
-
-/**
- * Created by Fiser on 9/10/16.
- */
 public class pruebaHibernate {
     public static void main(String[] args) {
-        System.out.println("Hibernate one to one (XML mapping)");
+        System.out.println("Hibernate many to many - join table + extra column (Annotation)");
         Session session = HibernateUtil.getSessionFactory().openSession();
+
         session.beginTransaction();
-        Habitacion stock = new Habitacion();
-        stock.setPlanta(5);
-        stock.setTipoHabitacion("Mala");
+
+        Stock stock = new Stock();
+        stock.setStockCode("7052");
+        stock.setStockName("PADINI");
+
+        Category category1 = new Category("CONSUMER", "CONSUMER COMPANY");
+        //new category, need save to get the id first
+        session.save(category1);
+
+        //Category category1 = (Category)session.get(Category.class, 8);
+
+        StockCategory stockCategory = new StockCategory();
+
+        stockCategory.setStock(stock);
+        stockCategory.setCategory(category1);
+        stockCategory.setCreatedDate(new Date());
+        stockCategory.setCreatedBy("system");
+
+        stock.getStockCategories().add(stockCategory);
+
         session.save(stock);
+
         session.getTransaction().commit();
         System.out.println("Done");
     }
-
 }

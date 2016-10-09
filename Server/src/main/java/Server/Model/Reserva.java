826ad4a3ac5bb130f0ee5256d6cfb08dd956a55b
Fiser <fiser12@icloud.com>
2016-10-09 16:52:37 +0200

    La relación intermedia aun no guarda datos

--- Server/src/main/java/Server/Model/Reserva.java
+++ Server/src/main/java/Server/Model/Reserva.java
@@ -1,6 +1,6 @@
 package Server.Model;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
--- Server/src/main/java/Server/pruebaHibernate.java
+++ Server/src/main/java/Server/pruebaHibernate.java
@@ -18,21 +18,24 @@ public class pruebaHibernate {
         stock.setTipoHabitacion("PADINI");
 
         Reserva category1 = new Reserva();
-        //new category, need save to get the id first
+        category1.setApellidos("Perosna1");
+        category1.setStartTime(new Date());
+        category1.setEndTime(new Date());
+        category1.setNombre("HOLA");
+        category1.setPrecio(4234);
+        category1.setTelefono(14231424);
         session.save(category1);
 
-        //Category category1 = (Category)session.get(Category.class, 8);
 
         HabitacionReserva stockCategory = new HabitacionReserva();
-
         stockCategory.setHabitacion(stock);
         stockCategory.setReserva(category1);
         stockCategory.setStartDate(new Date());
         stockCategory.setEndDate(new Date());
         stockCategory.setCreatedBy("system");
         stock.getHabitacionReservas().add(stockCategory);
+        category1.getHabitacionReservas().add(stockCategory);
         session.save(stock);
-
         session.getTransaction().commit();
         System.out.println("Done");
     }

226a93dbb091d61290278c86107de752b5d56e9d
Sergio Romero <srm@shkways.com>
2016-10-17 18:24:30 +0200

    Nueva estructura de servidor.

---
+++ Server/src/pruebaHibernate.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+import org.hibernate.Session;
+import model.Habitacion;
+import model.Reserva;
+import model.HabitacionReserva;
+
+public class pruebaHibernate {
+    public static void main(String[] args) {
+        System.out.println("Hibernate many to many - join table + extra column (Annotation)");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Habitacion stock = new Habitacion();
+        stock.setPlanta(12);
+        stock.setTipoHabitacion("PADINI");
+
+        Reserva category1 = new Reserva();
+        category1.setApellidos("Perosna1");
+        category1.setStartTime(new Date());
+        category1.setEndTime(new Date());
+        category1.setNombre("HOLA");
+        category1.setPrecio(4234);
+        category1.setTelefono(14231424);
+        session.save(category1);
+
+
+        HabitacionReserva stockCategory = new HabitacionReserva();
+        stockCategory.setHabitacion(stock);
+        stockCategory.setReserva(category1);
+        stockCategory.setStartDate(new Date());
+        stockCategory.setEndDate(new Date());
+        stockCategory.setCreatedBy("system");
+        stock.getHabitacionReservas().add(stockCategory);
+        category1.getHabitacionReservas().add(stockCategory);
+        session.save(stock);
+        session.getTransaction().commit();
+        System.out.println("Done");
+    }
+}

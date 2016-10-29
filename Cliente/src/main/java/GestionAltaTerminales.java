6c4604d145a5ce88b7e7ebc9fad1c26fadfd2c44
Sergio Romero <srm@shkways.com>
2016-10-29 20:28:04 +0200

    GestionAltaTerminales con llamada a los métodos mediante stub.

--- Cliente/src/main/java/GestionAltaTerminales.java
+++ Cliente/src/main/java/GestionAltaTerminales.java
@@ -1,12 +1,27 @@
 import java.rmi.RemoteException;
 
+import org.apache.ws.axis2.HelloService;
+import org.apache.ws.axis2.HelloServiceResponse;
+import org.apache.ws.axis2.ServicioHotelStub;
+
 /**
  * Created by Fiser on 4/10/16.
  */
 
 public class GestionAltaTerminales {
 
-    public static void main(String[] args){
+	public static void main(String[] args){
+		ServicioHotelStub stub;
+		try {
+			stub = new ServicioHotelStub("http://localhost:8080/axis2/services/HotelService");
 
-    }
+			HelloService hs = new HelloService();
+			HelloServiceResponse hsr = stub.helloService(hs);
+			System.out.println(hsr.get_return());
+
+		}catch (java.lang.Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }

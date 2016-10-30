7a9575d3f1ebe9b59026ba19c7b0a16231b3d1a8
Sergio Romero <srm@shkways.com>
2016-10-30 15:52:05 +0100

    Métodos para listar, obtener, actualizar y borrar reservas, pero al no funcionar el de crear no estoy seguro de su funcionamiento, en teoría deberían funcionar.

--- Cliente/src/main/java/GestionAltaTerminales.java
+++ Cliente/src/main/java/GestionAltaTerminales.java
@@ -2,11 +2,17 @@ import java.util.Date;
 
 import org.apache.ws.axis2.CreateReserva;
 import org.apache.ws.axis2.CreateReservaResponse;
+import org.apache.ws.axis2.DeleteReserva;
+import org.apache.ws.axis2.DeleteReservaResponse;
+import org.apache.ws.axis2.GetReserva;
+import org.apache.ws.axis2.GetReservaResponse;
 import org.apache.ws.axis2.HelloService;
 import org.apache.ws.axis2.HelloServiceResponse;
 import org.apache.ws.axis2.ListadoReserva;
 import org.apache.ws.axis2.ListadoReservaResponse;
 import org.apache.ws.axis2.ServicioHotelStub;
+import org.apache.ws.axis2.UpdateReserva;
+import org.apache.ws.axis2.UpdateReservaResponse;
 
 import model.xsd.Reserva;
 
@@ -16,17 +22,63 @@ import model.xsd.Reserva;
 
 public class GestionAltaTerminales {
 
-	public static void CrearReserva(ServicioHotelStub stub, Reserva r){
+	public static void crearReserva(ServicioHotelStub stub, Reserva r){
 		try {
 			CreateReserva nuevaReserva = new CreateReserva();
 			nuevaReserva.setReserva(r);
 			CreateReservaResponse nuevaReservaRes = stub.createReserva(nuevaReserva);
-			System.out.println(nuevaReservaRes.get_return());
+			System.out.println("Si sale true va bien: " + nuevaReservaRes.get_return());
 		}catch (java.lang.Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		
+	}
+	
+	public static void listarReservas(ServicioHotelStub stub){
+		try {
+			ListadoReserva lreserva = new ListadoReserva();
+			ListadoReservaResponse lReservaRes = stub.listadoReserva(lreserva);
+			System.out.println(lReservaRes.get_return());
+		}catch (java.lang.Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public static void obtenerReserva(ServicioHotelStub stub, Reserva r){
+		try {
+			GetReserva greserva = new GetReserva();
+			greserva.setId(r.getReservaId());
+			GetReservaResponse gReservaRes = stub.getReserva(greserva);
+			System.out.println("Si sale true lo ha encontrado: " +gReservaRes.get_return());
+		}catch (java.lang.Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public static void actualizarReserva(ServicioHotelStub stub, Reserva r){
+		try {
+			UpdateReserva ureserva = new UpdateReserva();
+			ureserva.setReserva(r);
+			UpdateReservaResponse uReservaRes = stub.updateReserva(ureserva);
+			System.out.println("Si sale true lo ha actualizado: " +uReservaRes.get_return());
+		}catch (java.lang.Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public static void borrarReserva(ServicioHotelStub stub, Reserva r){
+		try {
+			DeleteReserva dreserva = new DeleteReserva();
+			dreserva.setId(r.getReservaId());
+			DeleteReservaResponse dreservaRes = stub.deleteReserva(dreserva);
+			System.out.println("Si sale true lo ha borrado: " + dreservaRes.get_return());
+		}catch (java.lang.Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args){
@@ -37,7 +89,6 @@ public class GestionAltaTerminales {
 			HelloServiceResponse hsr = stub.helloService(hs);
 			System.out.println(hsr.get_return());
 			
-			//prueba nueva reserva
 			Reserva r = new Reserva();
 	        r.setApellidos("Prueba");
 	        r.setStartTime(new Date());
@@ -45,12 +96,34 @@ public class GestionAltaTerminales {
 	        r.setNombre("Prueba");
 	        r.setPrecio(1000);
 	        r.setTelefono(678964578);
-			CrearReserva(stub, r);
+	        
+	        Reserva r2 = new Reserva();
+	        r2.setApellidos("Prueba2");
+	        r2.setStartTime(new Date());
+	        r2.setEndTime(new Date());
+	        r2.setNombre("Prueba2");
+	        r2.setPrecio(15000);
+	        r2.setTelefono(678978978);
+			
+	        //prueba nueva reserva
+			crearReserva(stub, r);
+			crearReserva(stub, r2);
 			
 			//prueba listado
-			ListadoReserva lreserva = new ListadoReserva();
-			ListadoReservaResponse lreservaRes = stub.listadoReserva(lreserva);
-			System.out.println(lreservaRes.get_return());
+			listarReservas(stub);
+			
+			//prueba obtener una reserva concreta
+			obtenerReserva(stub, r2);
+			
+			/*prueba actualizar una reserva concreta; 
+			 * Supongo que aquí ya hemos seleccionado previamente la que queremos actualizar
+			 */
+			r2.setPrecio(25000); //finjo que desde la ventana he cambiado un dato
+			actualizarReserva(stub, r2); //envío un r2 cambiado con respecto al almacenado
+			
+			//prueba eliminar reserva
+			borrarReserva(stub, r);
+			
 		}catch (java.lang.Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();

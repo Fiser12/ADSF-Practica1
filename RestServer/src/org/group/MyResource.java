8b299f62768098bdb1392be4108d0e9f144707c8
Sergio Romero <srm@shkways.com>
2016-11-20 20:24:45 +0100

    Segunda entrega sin hibernate, da un fallo que podría ser un fallo de una librería.

---
+++ RestServer/src/org/group/MyResource.java
@@ -0,0 +1,179 @@
+package org.group;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.group.model.Habitacion;
+import org.group.model.HabitacionReserva;
+import org.group.model.Reserva;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/hotel")
+public class MyResource {
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent
+	 * to the client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 */
+
+	public static ArrayList<Reserva> reservas = new ArrayList<Reserva>();
+	public static ArrayList<Habitacion> habitaciones = new ArrayList<Habitacion>();
+	public static ArrayList<HabitacionReserva> habitacionesReserva = new ArrayList<HabitacionReserva>();
+
+
+	// This method is called if TEXT_PLAIN is request
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/hello")
+	public String sayPlainTextHello() {
+		return "Hello Jersey";
+	}
+
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/reserva/listado")
+	public Reserva[] listadoReserva(){
+		Reserva[] array = new Reserva[reservas.size()];
+		System.out.println(reservas.size());
+		array = reservas.toArray(array);
+		return array;
+	}
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/reserva/get/{id}")
+	public Reserva getReserva(int id){
+		for (Reserva reserva: reservas) {
+			if(reserva.getReservaId().equals(new Integer(id))){
+				return reserva;
+			}
+		}
+		return null;
+	}
+	@PUT
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/reserva/update")
+	public boolean updateReserva(Reserva reserva){
+		for (Reserva reservaTemp: reservas) {
+			if(reservaTemp.getReservaId().equals(reserva.getReservaId())){
+				reservaTemp.setApellidos(reserva.getApellidos());
+				reservaTemp.setEndTime(reserva.getEndTime());
+				reservaTemp.setStartTime(reserva.getStartTime());
+				reservaTemp.setNombre(reserva.getNombre());
+				reservaTemp.setNumeroPersonas(reserva.getNumeroPersonas());
+				reservaTemp.setTelefono(reserva.getTelefono());
+				reservaTemp.setPagado(reserva.getPagado());
+				reservaTemp.setPrecio(reserva.getPrecio());
+				reservaTemp.setTipoReserva(reserva.getTipoReserva());
+				return true;
+			}
+		}
+		return false;
+	}
+	@DELETE
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/reserva/delete/{id}")
+	public boolean deleteReserva(int id){
+		for(int i = 0; i<reservas.size(); i++){
+			if(reservas.get(i).getReservaId().equals(new Integer(id))){
+				reservas.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	@POST
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/reserva/create")
+	public boolean createReserva(Reserva reserva){
+		System.out.println(reserva.getNombre());
+		reservas.add(reserva);
+		return true;
+	}
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Consumes({"application/json"})
+	@Path("/habitacionesLibres")
+	public List<Habitacion> getHabitacionesLibres(SelectHabitacion valor){
+		List<Habitacion> devolver = new LinkedList<Habitacion>();
+		for (Habitacion habitacion: habitaciones) {
+			if(habitacion.getTipoHabitacion().equals(valor.tipoReserva))
+			{
+				boolean libre = true;
+				for(HabitacionReserva habitacionReserva: habitacionesReserva)
+				{
+					if(habitacionReserva.getHabitacion().getHabitacionID().equals(habitacion.getHabitacionID())){
+						if( valor.startDate.compareTo(habitacionReserva.getStartDate()) * habitacionReserva.getStartDate().compareTo(valor.endDate) > 0 || valor.startDate.compareTo(habitacionReserva.getEndDate()) * habitacionReserva.getEndDate().compareTo(valor.endDate) > 0)
+						{//Entra aquí si esa franja de tiempo de inicio y fin que ya está ocupada por una reserva en esa habitación
+							libre = false;
+						}
+					}
+				}
+				if(libre)
+				{
+					devolver.add(habitacion);
+				}
+			}
+		}
+		return devolver;
+	}
+	@GET
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/habitacionreserva/get")
+	public List<HabitacionReserva> getHabitacionesReserva(Reserva reserva)
+	{
+		List<HabitacionReserva> devolver = new LinkedList<HabitacionReserva>();       
+		for(HabitacionReserva habitacionReserva: habitacionesReserva)
+		{
+			if(habitacionReserva.getReserva().getReservaId()==reserva.getReservaId())
+			{
+				devolver.add(habitacionReserva);
+			}
+		}
+		return devolver;
+
+	}
+	@POST
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/habitacion/asignar")
+	public boolean asignarHabitacion(HabitacionReserva habitacionReserva){
+		for(int i = 0; i<habitacionesReserva.size(); i++){
+			if(habitacionesReserva.get(i).getHabitacion().getHabitacionID().equals(habitacionReserva.getHabitacion().getHabitacionID())&&habitacionesReserva.get(i).getReserva().getReservaId().equals(habitacionReserva.getReserva().getReservaId())){
+				return false;
+			}
+		}
+		habitacionesReserva.add(habitacionReserva);
+		return true;
+	}
+	@DELETE
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes({"application/json"})
+	@Path("/habitacion/desasignar")
+	public boolean desasignarHabitacion(HabitacionReserva habitacionReserva){
+		for(int i = 0; i<habitacionesReserva.size(); i++){
+			if(habitacionesReserva.get(i).getHabitacion().getHabitacionID().equals(new Integer(habitacionReserva.getHabitacion().getHabitacionID()))&&habitacionesReserva.get(i).getReserva().getReservaId().equals(new Integer(habitacionReserva.getReserva().getReservaId()))){
+				habitacionesReserva.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	@XmlRootElement
+	class SelectHabitacion{
+		public Date startDate;
+		public Date endDate;
+		public String tipoReserva;
+	}
+}
---
+++ RestServer/src/org/group/model/HabitacionReservaId.java
@@ -0,0 +1,50 @@
+package org.group.model;
+
+/**import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable**/
+public class HabitacionReservaId implements java.io.Serializable {
+
+	private Habitacion habitacion;
+    private Reserva reserva;
+
+//	@ManyToOne
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+
+//	@ManyToOne
+	public Reserva getReserva() {
+		return reserva;
+	}
+
+	public void setReserva(Reserva reserva) {
+		this.reserva = reserva;
+	}
+
+	public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+		HabitacionReservaId that = (HabitacionReservaId) o;
+
+        if (habitacion != null ? !habitacion.equals(that.habitacion) : that.habitacion != null) return false;
+        if (reserva != null ? !reserva.equals(that.reserva) : that.reserva != null)
+            return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = (habitacion != null ? habitacion.hashCode() : 0);
+        result = 31 * result + (reserva != null ? reserva.hashCode() : 0);
+        return result;
+    }
+    
+}
